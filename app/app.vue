f5a324d5b6b95f5cb750f6b1347f185aede7960b
Talkhestani <talkhestani@outlook.com>
2025-07-20 21:13:23 +0330

    📦 NEW: add landing page

--- app/app.vue
+++ app/app.vue
@@ -2,10 +2,7 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button class="btn btn-outline btn-secondary">
-    Secondary <Icon name="tabler:map" />
-  </button>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
